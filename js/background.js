9928d44caddb348dd5a59003c9d0b1312ffb6549
Aaron N. Brock <AaronNBrock@gmail.com>
2019-12-08 23:09:46 -0500

    Multiple profile support (#5)
    * add basic ui for profiles
    
    * the rest of the feature
    
    * Open profiles next to each other

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
- });
+});
 
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,3 +11,20 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   }]);
 });
 
+//receive message
+chrome.runtime.onMessage.addListener(function(message, sender) { 
+
+  chrome.tabs.create({
+    url: message.show_url,
+    active: false
+  }, function (tab) {
+    setTimeout(function(){ 
+      chrome.tabs.executeScript(tab.id, {
+        code: "console.log('Setting profile'); var profile = " + JSON.stringify(message.profile)
+      }, function () {
+        chrome.tabs.executeScript(tab.id, { file: "js/lottery.js" })
+      })
+    }, 5000);
+  });
+
+});
